d7cf6036aa0619898b8cabc97940ec4d85de3b19
Sindi Rojas <sindiangelica13@gmail.com>
2022-03-30 21:03:27 -0500

    Verificar Descuento

--- test/co/endava/app/AdministradorProductosTest.java
+++ test/co/endava/app/AdministradorProductosTest.java
@@ -1,10 +1,18 @@
 package co.endava.app;
 
+import co.endava.datos.Comida;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AdministradorProductosTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void main() {
+        Comida p3 = new Comida(103, "Pastel", 2000, LocalDate.now());
+        int descuento= (int) (p3.getPrecio()*0.1);
+        assertEquals(descuento, p3.getDescuento());
     }
 }
